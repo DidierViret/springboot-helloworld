86ffd12694db0655cb00af26bfee95da58923850
Didier Viret <didier.viret@orif.ch>
2024-09-08 22:34:20 +0200

    first commit

---
+++ src/test/java/ch/didierviret/springboot_helloworld/SpringbootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.didierviret.springboot_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
